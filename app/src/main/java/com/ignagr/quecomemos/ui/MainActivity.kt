a7740724000186acab64de22eb76c40fc678be6e
Igna Garcia <gnachoxp@gmail.com>
2021-11-23 19:28:31 -0300

    inir proyect

---
+++ app/src/main/java/com/ignagr/quecomemos/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ignagr.quecomemos.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ignagr.quecomemos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
