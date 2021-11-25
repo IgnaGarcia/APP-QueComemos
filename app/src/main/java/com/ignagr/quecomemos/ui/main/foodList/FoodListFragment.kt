ded05c0a0c0e1c4e1c15c0d1b91f1732d5673081
Igna Garcia <gnachoxp@gmail.com>
2021-11-25 01:43:19 -0300

    feat: food list

--- app/src/main/java/com/ignagr/quecomemos/ui/main/foodList/FoodListFragment.kt
+++ app/src/main/java/com/ignagr/quecomemos/ui/main/foodList/FoodListFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ignagr.quecomemos.R
 import com.ignagr.quecomemos.databinding.FragmentFoodListBinding
+import com.ignagr.quecomemos.entities.Food
+import com.ignagr.quecomemos.ui.main.foodSelection.FoodAdapter
 
 class FoodListFragment : Fragment(R.layout.fragment_food_list) {
     private var _binding: FragmentFoodListBinding? = null
     private val binding get() = _binding!!
+
+    private var foodAdapter : FoodAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +29,17 @@ class FoodListFragment : Fragment(R.layout.fragment_food_list) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentFoodListBinding.bind(view)
+
+        val food = Food("Milanesa con Pure", "Plato Principal", true, "")
+        chargeFoodList(listOf(food, food, food, food, food, food, food))
+
+    }
+
+    private fun chargeFoodList(mapTravelList: List<Food>) {
+        foodAdapter = FoodAdapter(mapTravelList)
+        binding.rvFood.adapter = foodAdapter
+        binding.rvFood.layoutManager = LinearLayoutManager(
+            requireActivity(), LinearLayoutManager.VERTICAL, false)
     }
 
     override fun onDestroy() {
